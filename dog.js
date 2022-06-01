d7cb48b12979388d397e4926f71aa0d981624253
Indykatz <cats83@me.com>
2022-06-01 16:54:34 +0100

    cloned from Gaz

---
+++ dog.js
@@ -0,0 +1,45 @@
+const { Pet } = require("./pet");
+
+class Dog extends Pet {
+  constructor(name) {
+      super(name)
+  }
+
+  walk(){
+      this.hunger -= 5;
+      this.thirst -= 5;
+      this.health += 5;
+      this.happiness += 10;
+      this.hygiene -= 10;
+      this.energy -= 10;
+      console.log(`${myPet.name} Loves going on walks! ${myPet.name} is now getting sleepy`);
+     /* console.log(",-.___,-.")
+      console.log(`\_/_ _\_/`)
+      console.log("  )O_O(")
+      console.log(" { (_) }")
+      console.log("  `-^-' ") */
+  }
+
+  playTug(){
+      this.hunger -= 5;
+      this.thirst -= 5;
+      this.health += 5;
+      this.happiness += 10;
+      this.hygiene -= 5;
+      this.energy -= 10;
+      console.log(`${myPet.name} Loves playing tug! ${myPet.name} is now getting sleepy`);
+  }
+
+  playFetch(){
+      this.hunger -= 5;
+      this.thirst -= 5;
+      this.health += 5;
+      this.happiness += 10;
+      this.hygiene -= 5;
+      this.energy -= 10;
+      console.log(`${myPet.name} Loves playing fetch! ${myPet.name} is now getting sleepy`);
+  }
+}
+
+module.exports = { Dog };
+
